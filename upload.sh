bb8cad3bd31bd67aa5b920d99e27383bc172594b
Awni Hannun <awni@fb.com>
2020-08-31 16:41:22 -0400

    add fair link, fix upload

--- upload.sh
+++ upload.sh
@@ -6,14 +6,14 @@ then
     # uncomment to also upload pdfs
     for dir in "assets" "research" # "writing" "tutorials" "papers" "talks"
     do
-        aws2 s3 cp --recursive $dir s3://awnihannun.com/$dir
+        aws s3 cp --recursive $dir s3://awnihannun.com/$dir
     done
     for f in `ls *.html`
     do
-        aws2 s3 cp $f s3://awnihannun.com/
+        aws s3 cp $f s3://awnihannun.com/
     done
 else 
     echo "Uploading $1";
-    aws2 s3 cp $1 s3://awnihannun.com/$1
+    aws s3 cp $1 s3://awnihannun.com/$1
 fi
 
